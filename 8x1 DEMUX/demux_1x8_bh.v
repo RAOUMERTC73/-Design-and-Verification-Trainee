22c1d9a1bfbf0f0dc65a6b710b054d5a103200c0
RAOUMERTC73 <raoumer160903@gmail.com>
2024-02-16 21:10:57 +0500

    2nd week feb  remove some bugs in code

--- 8x1 DEMUX/demux_1x8_bh.v
+++ 8x1 DEMUX/demux_1x8_bh.v
@@ -1,24 +1,18 @@
 `timescale 1ns / 1ps
 
 module demux_1x8_bh(
-    output reg [7:0]y,
+    output reg y0, y1, y2, y3, y4, y5, y6, y7,
     input i,
     input [2:0] s
     );
-    always@(*)
-    begin
-        y=0;
-        case(s)
-            3'b000: y[0]=i;
-            3'b001: y[1]=i;
-            3'b010: y[2]=i;
-            3'b011: y[3]=i;
-            3'b100: y[4]=i;
-            3'b101: y[5]=i;
-            3'b110: y[6]=i;
-            3'b111: y[7]=i;
-            default: y[7:0]=8'bx;
-        endcase
+    always @(*) begin
+        y0 = (s == 3'b000) ? i : 1'b0;
+        y1 = (s == 3'b001) ? i : 1'b0;
+        y2 = (s == 3'b010) ? i : 1'b0;
+        y3 = (s == 3'b011) ? i : 1'b0;
+        y4 = (s == 3'b100) ? i : 1'b0;
+        y5 = (s == 3'b101) ? i : 1'b0;
+        y6 = (s == 3'b110) ? i : 1'b0;
+        y7 = (s == 3'b111) ? i : 1'b0;
     end
 endmodule
-
