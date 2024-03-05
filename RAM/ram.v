eb59e25498c62dd22b3c8166b9388df30a040e86
RAOUMERTC73 <raoumer160903@gmail.com>
2024-03-05 23:12:25 +0500

    updated  RAM  compare some comb ckt

--- RAM/ram.v
+++ RAM/ram.v
@@ -25,3 +25,19 @@ end
 assign dout = mem[adr];
 
 endmodule
+
+
+/**************************************************
+ P1.1 4:1 MULTIPLEXER DATAFLOW1
+ ************************************************/
+
+ module mux_4_1_df(Y,I,S);
+ 	input [3:0]I;
+ 	input [1:0]S;
+ 	output Y;
+
+ assign Y= I[S];
+
+
+ endmodule
+
